73b016145d21cf9eca766b50496ec66e0ed3e40d
toanvv.ti.1922@gmail.com <toanvv.ti.1922@gmail.com>
2024-10-17 23:50:09 +0700

    Refactor state.py to include missing constants in state representation

--- Project 1/22125038_22125049_22125077_22125108/Source/state/__init__.py
+++ Project 1/22125038_22125049_22125077_22125108/Source/state/__init__.py
@@ -1,7 +1,10 @@
 from .state import SearchState
+from .state import WALL, EMPTY, STONE, AGENT, SWITCH, STONE_SWITCH, AGENT_SWITCH
 from .output import SearchOutput
+
 
 __all__ = [
     'SearchState',
-    'SearchOutput'
+    'SearchOutput',
+    "WALL", "EMPTY", "STONE", "AGENT", "SWITCH", "STONE_SWITCH", "AGENT_SWITCH"
 ]
