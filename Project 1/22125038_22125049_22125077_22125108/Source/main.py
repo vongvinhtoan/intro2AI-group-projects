1842606bfdf546dce34720a20502ea3c8cf970f3
toanvv.ti.1922@gmail.com <toanvv.ti.1922@gmail.com>
2024-10-17 23:07:38 +0700

    Refactor main.py to support multiple strategies and input/output files

--- Project 1/22125038_22125049_22125077_22125108/Source/main.py
+++ Project 1/22125038_22125049_22125077_22125108/Source/main.py
@@ -1,10 +1,32 @@
-from searchstate import *
+from state import *
 from strategy import *
 
-import argparse
+def parse_args():
+    import argparse
+    return {
+        'strategies': ['DFS', 'BFS', 'UCS', 'A*'],
+        'input_files': ['input-01.txt'],
+        'output_files': ['output-01.txt']
+    }
 
 def main():
-    pass
+    args = parse_args()
+    strategies = args['strategies']
+    input_files = args['input_files']
+    output_files = args['output_files']
+
+    solvers = [solver_dict[strategy] for strategy in strategies]
+    
+    for input_file, output_file in zip(input_files, output_files):
+        state = SearchState(input_file)
+
+        # Clear the output file
+        open(output_file, 'w').close()
+
+        for solver in solvers:
+            solver.set_state(state)
+            output = solver.search()
+            output.write(output_file)
 
 if __name__ == '__main__':
     main()
